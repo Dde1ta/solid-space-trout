da4d524641203c2e00a7f1cbd8dd552414439000
Dde1ta <86724438+Dde1ta@users.noreply.github.com>
2022-12-06 05:02:08 +0530

    Update potter.py

--- Grapher_me/potter.py
+++ Grapher_me/potter.py
@@ -8,46 +8,50 @@ class Graph_frame:
         self.master = master
         self.HEIGHT = 1000
         self.WIDHT = 1000
-        self.coords_label = tk.Label(self.master, bg="white")
+
         master.geometry(str(self.master.winfo_screenwidth()) + 'x' + str(self.master.winfo_screenheight()))
         master.state('zoomed')
-        self.canvas = tk.Canvas(master, width=self.WIDHT, height=self.HEIGHT,highlightthickness=1,
-                                highlightbackground="white",bg = "black")
+        self.graph_frame = tk.Frame(self.master,height=self.HEIGHT,width = self.WIDHT)
+        self.graph_canvas = tk.Canvas(self.graph_frame, width=self.WIDHT, height=self.HEIGHT, highlightthickness=1,
+                                      highlightbackground="white", bg = "black")
+        self.coords_label = tk.Label(self.graph_frame, bg="white")
         print(self.master.winfo_screenheight())
-        self.canvas.create_line(500,0,500,1000,width=2,fill = "white") #Y_axis
-        self.canvas.create_line(0,500,1000,500, width=2,fill = "white") #X_axis
+        self.graph_canvas.create_line(500, 0, 500, 1000, width=2, fill ="white") #Y_axis
+        self.graph_canvas.create_line(0, 500, 1000, 500, width=2, fill ="white") #X_axis
         self.hex_list = ['1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f']
+        #___________________________________________________________________________________________________________________________________
+
+        #___________________________________________________________________________________________________________________________________
         self.y_axis_values()
         self.x_axis_values()
         self.vertical_grid()
         self.horizontal_grid()
-        self.canvas.place(x = 0,y = 0)
+        self.graph_canvas.place(x = 0, y = 0)
         self.place_coords_label()
-        self.input_taker()
     def vertical_grid(self):
         for i in range(0,1000,50):
             if i == 500:
                 pass
             else:
-                self.canvas.create_line(i,0,i,1000,width=0.01,fill="gray")
+                self.graph_canvas.create_line(i, 0, i, 1000, width=0.01, fill="gray")
     def horizontal_grid(self):
         for i in range(0,1000,50):
             if i == 500:
                 pass
             else:
-                self.canvas.create_line(0,i,1000,i,fill="gray")
+                self.graph_canvas.create_line(0, i, 1000, i, fill="gray")
     def y_axis_values(self):
-        self.y_axis_values_list_positive = [tk.Label(self.master,text = i,bg = "black",fg = "white") for i in range(10,-1,-1)]
-        self.y_axis_values_list_negative = [tk.Label(self.master,text = i,bg = "black",fg = "white") for i in range(-1,-11,-1)]
+        self.y_axis_values_list_positive = [tk.Label(self.graph_frame,text = i,bg = "black",fg = "white") for i in range(10,-1,-1)]
+        self.y_axis_values_list_negative = [tk.Label(self.graph_frame,text = i,bg = "black",fg = "white") for i in range(-1,-11,-1)]
         for n in range(11):
             self.y_axis_values_list_positive[n].place(x = 510,y = n*50-10)
 
         for n in range(10):
             self.y_axis_values_list_negative[n].place(x=510, y=((n+11) * 50)-14)
     def x_axis_values(self):
-        self.x_axis_values_list_positive = [tk.Label(self.master, text=i, bg="black", fg="white") for i in
+        self.x_axis_values_list_positive = [tk.Label(self.graph_frame, text=i, bg="black", fg="white") for i in
                                             range(1,10)]
-        self.x_axis_values_list_negative = [tk.Label(self.master, text=i, bg="black", fg="white") for i in
+        self.x_axis_values_list_negative = [tk.Label(self.graph_frame, text=i, bg="black", fg="white") for i in
                                             range(-10,0)]
         for n in range(10):
             self.x_axis_values_list_negative[n].place(x = 50*n -8,y = 510)
@@ -62,12 +66,12 @@ class Graph_frame:
         line_color = self.random_color()
         for i in range(0, len(self.points_list)):
             try:
-                self.canvas.create_line(self.points_list[i][0]+500,
-                    -1*self.points_list[i][1]+500,
-                    self.points_list[i+1][0]+500,
-                    -1*self.points_list[i+1][1]+500,fill = line_color)
+                self.graph_canvas.create_line(self.points_list[i][0] + 500,
+                                              -1 * self.points_list[i][1] + 500,
+                                              self.points_list[i+1][0] + 500,
+                                              -1 * self.points_list[i+1][1] + 500, fill = line_color)
             except:
-                self.canvas.update()
+                self.graph_canvas.update()
     def on_canvas(self,cords):
         if cords[0] <1000 and cords[1] <1000:
             return True
@@ -77,30 +81,26 @@ class Graph_frame:
         self.mouse = pa.position()
         self.mouse_coords = [self.mouse.x, self.mouse.y]
         if self.on_canvas(self.mouse_coords):
-            self.canvas.delete("coords")
-            self.canvas.create_text(self.mouse_coords[0]+20,self.mouse_coords[1] - 50,
-                                    text=str((self.mouse_coords[0] - 500)/50)+","+str(-1*((self.mouse_coords[1]-30) - 500)/50),
-                                    fill = 'white',
-                                    tag = "coords")
+            self.graph_canvas.delete("coords")
+            self.graph_canvas.create_text(self.mouse_coords[0] + 20, self.mouse_coords[1] - 50,
+                                          text=str((self.mouse_coords[0] - 500)/50)+","+str(-1*((self.mouse_coords[1]-30) - 500)/50),
+                                          fill = 'white',
+                                          tag = "coords")
         else:
             pass
-        self.master.after(1,self.place_coords_label)
+        self.graph_frame.after(1,self.place_coords_label)
     def random_color(self):
         colour_code = "#"
         for i in range(6):
             colour_code += r.choice(self.hex_list)
         return colour_code
-    def input_taker(self):
-        self.input_canvas = tk.Canvas(self.master, width=self.WIDHT, height=self.HEIGHT,highlightthickness=1,
-                                highlightbackground="white",bg = "black")
-        self.input_canvas.place(x= self.WIDHT,y = 0)
-
 if __name__ == "__main__":
-    master = tk.Tk()
-    gr = Graph_frame(master)
+    graph_frame = tk.Tk()
+    gr = Graph_frame(graph_frame)
+    gr.graph_frame.place(x = 0,y= 0 )
     # gr.plot(["x","power",2,"add",2,"multiply","x"])
     # gr.plot(["x", "power", 3, "add", 2, "multiply", "x"])
     gr.plot(["x", "power", 4, "add", 2, "multiply", "x"])
     # gr.plot(["x","power",5,"add",2,"multiply","x"])
 
-    master.mainloop()
+    graph_frame.mainloop()
