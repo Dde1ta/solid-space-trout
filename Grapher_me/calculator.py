0e79cb6bd7005fb5d90156c22d22d2b4caca628e
Dde1ta <divyanshmanhasdm2005@gmail.com>
2023-01-19 17:50:39 +0530

    Changing calculator

--- Grapher_me/calculator.py
+++ Grapher_me/calculator.py
@@ -1,2 +1,14 @@
+"""
+Returns the list of points:->
+    the graph x_axis is from -10 to 10 : 20 squares
+    there are 500 pixles: each square has 25px (0.04,0.08,0.12,0.16,...,1,2,...,10)
+    x_px = (-250,250): So x = x_px / 25
+    That is how we calculate the points
+"""
 class Calculator:
-    def
+    def __init__(self,string):
+        self.equation_string = string
+        self._x_px = -250
+        self.x_px = 250
+        self.pixles = 25
+        self.answer_list = []
